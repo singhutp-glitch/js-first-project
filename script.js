aad338a5ff488134ac8a02ddc046cc07c7e84605
singhutp-glitch <singhutp@gmail.com>
2025-08-17 15:48:25 +0530

    Add function for computerchoice

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("hello world");
+
+function getcomputerchoice()
+{
+    randnum = Math.floor(Math.random()*3);
+    if(randnum ===0)
+    {
+        return "rock";
+    }
+    else if(randnum===1)
+    {
+        return "paper";
+    }
+    else
+    {
+        return "scissors";
+    }
+}
+
+console.log(getcomputerchoice());
