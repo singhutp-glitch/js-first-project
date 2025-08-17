f3dc85f078b9dd448a14f6e0db2764f13c91dceb
singhutp-glitch <singhutp@gmail.com>
2025-08-17 16:58:06 +0530

    Add logic for one round play
    -take choice of human and computer
    -encode them
    -evaluate winner
    -show round result
    -increment score

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("hello world");
-
+//functions
 function getcomputerchoice()
 {
-    randnum = Math.floor(Math.random()*3);
+    let randnum = Math.floor(Math.random()*3);
     if(randnum ===0)
     {
         return "rock";
@@ -21,6 +21,47 @@ function gethumanchoice()
 {
     return prompt("enter your choice ","rock");
 }
+function encode(choice)
+{
+    if(choice==="rock")
+    {
+        return 0;
+    }
+    else if(choice==="paper")
+    {
+        return 1;
+    }
+    else
+    {
+        return 2;
+    }
+}
 
-console.log(gethumanchoice());
-console.log(getcomputerchoice());
+function playround()
+{
+    const humanselection=gethumanchoice().toLowerCase();
+    const humancode=encode(humanselection);
+    const compselection=getcomputerchoice();
+    const compcode=encode(compselection);
+
+    if(humancode-compcode===0)
+    {
+        console.log(humanselection+" = "+compselection+" -> Draw");
+    }
+    else if((humancode-compcode+3)%3===1)
+    {
+        console.log(humanselection+" beats "+compselection+" -> you win");
+        humanscore++;
+    }
+    else
+    {
+        console.log(compselection+" beats "+humanselection+" -> computer wins");
+        computerscore++;
+    }
+
+}
+
+//main
+let humanscore=0;
+let computerscore=0;
+playround();
